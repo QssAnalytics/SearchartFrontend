9a0812f759db12e8ec8d35f2e8367750d6ad342b
Fuad <fuadbayramv@gmail.com>
2023-05-20 14:50:30 +0400

    added design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,16 @@ export default {
 
       colors: {
         searchart: {
+          primary: "#CC3203",
+          stroke: "#727272",
           background: "#ffffff",
+          text: "#030712",
+          footer: "#F6F6F8",
+          "footer-text": "#9DA2AA",
           dark: {
             background: "linear-gradient(to bottom,#0A1525 100%,#041023 100%)",
+            text: "#D0D2D6",
+            footer: "#182842",
           },
         },
       },
